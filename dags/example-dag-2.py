b5c66001a910804218b04c402931542176b0822f
Kenten Danas <kentendanas@Kentens-MacBook-Pro.local>
2021-05-25 12:58:23 -0700

    Test GH actions again

--- dags/example-dag-2.py
+++ dags/example-dag-2.py
@@ -22,7 +22,7 @@ default_args = {
     'depends_on_past': False,
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
+    'retries': 0,
     'retry_delay': timedelta(minutes=5)
 }
 
