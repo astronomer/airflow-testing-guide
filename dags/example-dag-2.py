828453075ab1ab21cecda1243eefb2b3420b480d
Kenten Danas <kentendanas@Kentens-MacBook-Pro.local>
2021-05-25 13:35:27 -0700

    Fix tests to pass

--- dags/example-dag-2.py
+++ dags/example-dag-2.py
@@ -22,7 +22,7 @@ default_args = {
     'depends_on_past': False,
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 0,
+    'retries': 2,
     'retry_delay': timedelta(minutes=5)
 }
 
