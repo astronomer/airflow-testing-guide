c8b3ba7ceb302ca32b3456c78d445758f77eea0d
Kenten Danas <kentendanas@Kentens-MacBook-Pro.local>
2021-05-25 11:49:20 -0700

    Airflow unit tests example

---
+++ include/test_dag_validation.py
@@ -0,0 +1,14 @@
+import pytest
+from airflow.models import DagBag
+
+def test_no_import_errors():
+  dag_bag = DagBag()
+  assert len(dag_bag.import_errors) == 0, "No Import Failures"
+
+
+def test_retries_present():
+  dag_bag = DagBag()
+  for dag in dag_bag.dags:
+      retries = dag_bag.dags[dag].default_args.get('retries', [])
+      error_msg = 'Retries not set to 2 for DAG {id}'.format(id=dag)
+      assert retries == 2, error_msg
